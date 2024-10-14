8305fa0eb7261d5ff3bb8d43cc7a8f1504b0d2d5
CruzM-224 <cruzmoreno022002@gmail.com>
2024-10-13 21:41:25 -0600

    Edicion de datos

--- App/editResource.jsx
+++ App/editResource.jsx
@@ -3,57 +3,37 @@ import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button, StyleSheet, Text, ScrollView } from 'react-native';
 import axios from 'axios';
 import useStore from '../components/useStore';
-import { useRoute } from '@react-navigation/native';
 import { Alert } from 'react-native';
+import { router } from 'expo-router';
 
 
 const EditScreen = () => {
-  const route = useRoute(); // Utiliza useRoute para acceder a las props de navegación
-  const { resource } = route.params || {}; // Extrae el recurso de los params
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [type, setType] = useState('');
-    const [url, setUrl] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
+  const resource = useStore((state) => state.searchedResourceInStore);
+  const [title, setTitle] = useState(resource?.titulo || '');
+  const [description, setDescription] = useState(resource?.descripcion || '');
+  const [type, setType] = useState(resource?.tipo || '');
+  const [url, setUrl] = useState(resource?.enlace || '');
+  const [imageUrl, setImageUrl] = useState(resource?.imagen || '');
 
-    const resources = useStore((state) => state.resources);
-    const setResources = useStore((state) => state.setResources);
+  function goBack() {
+    router.back();
+  }
 
-    /* // 2. SE AGREGA: useEffect para cargar los datos del recurso seleccionado al montar el componente.
-     useEffect(() => {
-      if (resources) { // Verificamos si existe un recurso para evitar errores.
-          setTitle(resources.titulo);
-          setDescription(resources.descripcion);
-          setType(resources.tipo);
-          setUrl(resources.enlace);
-          setImageUrl(resources.imagen);
-      }
-    }, [resources]); // Dependencia: se ejecuta cada vez que "resource" cambia.*/
-
-    async function handleUpdateResource() {
-        try {
-            const response = await axios.put('https://670801878e86a8d9e42dc45a.mockapi.io/api/recursos/${resource.id}', {
-                titulo: title,
-                descripcion: description,
-                tipo: type,
-                enlace: url,
-                imagen: imageUrl,
-            });
-
-              // 4. SE AGREGA: Actualizar el estado global de recursos con la respuesta del servidor.
-              setResources((prevResources) =>
-                prevResources.map((res) => (res.id === response.data.id ? response.data : res))
-            );
-
-            // Mostrar una alerta de éxito
-            Alert.alert('Éxito', 'Recurso modificado con éxito');
-
-        } catch (error) {
-            console.error(error);
-            Alert.alert('Error', 'No se pudo modificar el recurso. Intente nuevamente.');
-          
-        }
+  async function handleUpdateResource() {
+    try {
+      const response = await axios.put(`https://670801878e86a8d9e42dc45a.mockapi.io/api/recursos/${resource.id}`, {
+        titulo: title,
+        descripcion: description,
+        tipo: type,
+        enlace: url,
+        imagen: imageUrl,
+      });
+      console.log(response.data);
+      goBack();
+    } catch (error) {
+        console.error(error); 
     }
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -119,7 +99,7 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 22,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10,
     textAlign: 'center',
   },
   inputLabel: {
--- components/useStore.js
+++ components/useStore.js
@@ -3,6 +3,8 @@ import { create } from 'zustand'
 const useStore = create((set) => ({
     resources: [],
     setResources: (resources) => set({ resources }),
+    searchedResourceInStoreInStore: null,
+    setSearchedResourceInStore: (searchedResourceInStore) => set({ searchedResourceInStore }),
 }))
 
 export default useStore;
